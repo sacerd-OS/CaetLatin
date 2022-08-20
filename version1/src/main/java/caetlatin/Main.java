910a94d50e1ec87a0a61fce92fe1a9d092d28342
August Huber <augustalexander27@gmail.com>
2022-08-19 23:34:12 -0400

    removing build tools from release branch

--- version1/src/main/java/caetlatin/Main.java
+++ version1/src/main/java/caetlatin/Main.java
@@ -1,16 +1,7 @@
 package caetlatin;
 class Main{
     public static void main(String args[]){
-        boolean build = false; // true = enter build mode; false = enter dictionary mode
-
-        if(build){
-            JSONBuilder j = new JSONBuilder();
-            j.buildJSON();
-        }
-
-        else{
-            CaetLatin caet = new CaetLatin();
-            caet.viewer();
-        }
+        CaetLatin caet = new CaetLatin();
+        caet.viewer();
     }
 }
